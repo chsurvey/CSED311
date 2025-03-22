08f990faa9c837da98a8e85fda2e376616ffcbdb
chsurvey <justin80219195@gmail.com>
2025-03-22 16:03:16 +0900

    fix: alu rollback

--- Lab2/alu.v
+++ Lab2/alu.v
@@ -1,92 +1,16 @@
 module alu(
-    alu_op,
-    alu_in_1,
-    alu_in_2,
-    alu_result,
-    alu_bcond
+    input [4:0] alu_op,
+    input [31:0] alu_in_1,
+    input [31:0] alu_in_2,
+    output reg [31:0] alu_result,
+    output reg alu_bcond
 );
-
-    input [4:0] alu_op;
-    input [31:0] alu_in_1;
-    input [31:0] alu_in_2;
-    output reg [31:0] alu_result;
-    output reg alu_bcond;
-
     always @(*) begin
         alu_result = 0;
         alu_bcond = 0;
     
         case(alu_op)
-            5'b00000: // Always 0
-                alu_result = 0;
             
-            5'b00001: // Always 1
-                alu_result = 1;
-            
-            5'b00010: // Identity
-                alu_result = alu_in_1;
-            
-            5'b00011: // Addition
-                alu_result = alu_in_1 + alu_in_2;
-            
-            5'b00100: // Subtraction
-                alu_result = alu_in_1 - alu_in_2;
-
-            5'b00101: // Increment
-                alu_result = alu_in_1 + 1;
-
-            5'b00110: // Decrement
-                alu_result = alu_in_1 - 1;
-
-            5'b00111: // One's complement
-                alu_result = ~alu_in_1;
-            
-            5'b01000: // Two's complement
-                alu_result = ~alu_in_1 + 1;
-
-            5'b01001: // AND
-                alu_result = alu_in_1 & alu_in_2;
-
-            5'b01010: // OR
-                alu_result = alu_in_1 | alu_in_2;
-            
-            5'b01011: // NAND
-                alu_result = ~(alu_in_1 & alu_in_2);
-
-            5'b01100: // NOR
-                alu_result = ~(alu_in_1 | alu_in_2);
-            
-            5'b01101: // XOR
-                alu_result = alu_in_1 ^ alu_in_2;
-
-            5'b01110: // NXOR
-                alu_result = ~(alu_in_1 ^ alu_in_2);
-            
-            5'b01111: // Logical shift left
-                alu_result = alu_in_1 << alu_in_2[4:0];
-            
-            5'b10000: // Logical shift right
-                alu_result = alu_in_1 >> alu_in_2[4:0];
-
-            5'b10001: // Arithmetic shift left
-                alu_result = alu_in_1 <<< alu_in_2[4:0];
-            
-            5'b10010: // Arithmetic shift right
-                alu_result = alu_in_1 >>> alu_in_2[4:0];
-
-            5'b10011: // BEQ
-                alu_bcond = alu_in_1 == alu_in_2;
-            
-            5'b10100: // BNE
-                alu_bcond = alu_in_1 != alu_in_2;
-
-            5'b10101: // BLT
-                alu_bcond = alu_in_1 < alu_in_2;
-            
-            5'b10110: // BGE
-                alu_bcond = alu_in_1 >= alu_in_2;
-            default: begin
-            end
         endcase
     end
 endmodule
--- Lab2/alu_control_unit.v
+++ Lab2/alu_control_unit.v
@@ -1,12 +1,9 @@
 `include "opcodes.v"
 
 module alu_control_unit (
-    part_of_inst,
-    alu_op
+    input[16:0] part_of_inst,
+    output reg[4:0] alu_op
   );
-    input [16:0] part_of_inst;
-    output reg [4:0] alu_op;
-
     wire [6:0] funct7;
     wire [2:0] funct3;
     wire [6:0] opcode;
@@ -17,79 +14,35 @@ module alu_control_unit (
 
     always @(*) begin        
         alu_op = 5'b00000;
+        case(opcode)
+            `ARITHMETIC: begin
+                if (funct7 == `FUNCT7_OTHERS) begin
+                    case (funct3)
+                        `FUNCT3_ADD: alu_op = // alu op code
+                        `FUNCT3_AND: alu_op
+                        `FUNCT3_OR: alu_op
+                        `FUNCT3_XOR: alu_op
+                        `FUNCT3_SLL: alu_op
+                        `FUNCT3_SRL: alu_op
+                        default: begin
+                        end
+                    endcase
+                end
 
-        if (opcode == `ARITHMETIC) begin
-            if (funct7 == `FUNCT7_OTHERS) begin
-                case (funct3)
-                    `FUNCT3_ADD: alu_op = 5'b00011;
-                    `FUNCT3_AND: alu_op = 5'b01001;
-                    `FUNCT3_OR: alu_op = 5'b01010;
-                    `FUNCT3_XOR: alu_op = 5'b01101;
-                    `FUNCT3_SLL: alu_op = 5'b01111;
-                    `FUNCT3_SRL: alu_op = 5'b10000;
-                    default: begin
-                    end
-                endcase
+                if (funct7 == `FUNCT7_SUB) begin
+                    case (funct3)
+                        `FUNCT3_SUB: alu_op = 5'b00100;
+                        default: begin
+                        end
+                    endcase
+                end
             end
-
-            if (funct7 == `FUNCT7_SUB) begin
-                case (funct3)
-                    `FUNCT3_SUB: alu_op = 5'b00100;
-                    default: begin
-                    end
-                endcase
-            end
-        end
-
-        if (opcode == `ARITHMETIC_IMM) begin
-
-            case(funct3)
-                `FUNCT3_ADD: alu_op = 5'b00011;
-                `FUNCT3_AND: alu_op = 5'b01001;
-                `FUNCT3_OR: alu_op = 5'b01010;
-                `FUNCT3_XOR: alu_op = 5'b01101;
-                `FUNCT3_SLL: alu_op = 5'b01111;
-                `FUNCT3_SRL: alu_op = 5'b10000;
-                default: begin
-                end
-            endcase
-        
-        end
-
-        if (opcode == `LOAD) begin
-            case(funct3)
-                `FUNCT3_LW: alu_op = 5'b00011;
-                default: begin
-                end
-            endcase
-        end
-
-        if (opcode == `STORE) begin
-            case(funct3)
-                `FUNCT3_SW: alu_op = 5'b00011;
-                default: begin
-                end
-            endcase
-        end
-
-        if (opcode == `BRANCH) begin
-            case(funct3)
-                `FUNCT3_BEQ: alu_op = 5'b10011;
-                `FUNCT3_BNE: alu_op = 5'b10100;
-                `FUNCT3_BLT: alu_op = 5'b10101;
-                `FUNCT3_BGE: alu_op = 5'b10110;
-                default: begin
-                end
-            endcase
-        end
-
-        if (opcode == `JALR) begin
-            alu_op = 5'b00011;
-        end
-
-        if (opcode == `JAL) begin
-            alu_op = 5'b00011;
-        end
-
+            `ARITHMETIC_IMM:
+            `LOAD:
+            `JALR:
+            `STORE:
+            `BRANCH:
+            `JAL:
+        endcase
     end
 endmodule
