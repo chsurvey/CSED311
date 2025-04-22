5b3bc502bb79ab85cd954cf94933298adce5f085
chsurvey <--global>
2025-04-22 20:35:29 +0900

    fetch: midpoint push

--- Lab4-1/cpu.v
+++ Lab4-1/cpu.v
@@ -13,13 +13,20 @@ module cpu(input reset,       // positive reset signal
            output is_halted, // Whehther to finish simulation
            output [31:0]print_reg[0:31]); // Whehther to finish simulation
   /***** Wire declarations *****/
+  wire [31:0] current_pc;
+  wire [31:0] next_pc;
+  wire [31:0] inst;
+  wire [31:0] rs1_dout;
+  wire [31:0] rs2_dout;
+  wire [31:0] rd_din;
+
   /***** Register declarations *****/
   // You need to modify the width of registers
   // In addition, 
   // 1. You might need other pipeline registers that are not described below
   // 2. You might not need registers described below
   /***** IF/ID pipeline registers *****/
-  reg IF_ID_inst;           // will be used in ID stage
+  reg [31:0]IF_ID_inst;           // will be used in ID stage
   /***** ID/EX pipeline registers *****/
   // From the control unit
   reg ID_EX_alu_op;         // will be used in EX stage
@@ -58,65 +65,91 @@ module cpu(input reset,       // positive reset signal
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
   PC pc(
-    .reset(),       // input (Use reset to initialize PC. Initial value must be 0)
-    .clk(),         // input
-    .next_pc(),     // input
-    .current_pc()   // output
+    .reset(reset),       // input (Use reset to initialize PC. Initial value must be 0)
+    .clk(clk),         // input
+    .next_pc(next_pc),     // input
+    .current_pc(current_pc)   // output
   );
   
   // ---------- Instruction Memory ----------
   InstMemory imem(
-    .reset(),   // input
-    .clk(),     // input
-    .addr(),    // input
-    .dout()     // output
+    .reset(reset),   // input
+    .clk(clk),     // input
+    .addr(current_pc),    // input
+    .dout(inst)     // output
   );
 
   // Update IF/ID pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      IF_ID_inst <= 32'b0;
     end
     else begin
+      IF_ID_inst <= inst;
     end
   end
 
   // ---------- Register File ----------
   RegisterFile reg_file (
-    .reset (),        // input
-    .clk (),          // input
-    .rs1 (),          // input
-    .rs2 (),          // input
-    .rd (),           // input
-    .rd_din (),       // input
-    .write_enable (),    // input
-    .rs1_dout (),     // output
-    .rs2_dout (),      // output
+    .reset (reset),        // input
+    .clk (clk),          // input
+    .rs1 (IF_ID_inst[19:15]),          // input
+    .rs2 (IF_ID_inst[24:20]),          // input
+    .rd (IF_ID_inst[11:7]),           // input
+    .rd_din (rd_din),       // input
+    .write_enable (MEM_WB_reg_write),    // input
+    .rs1_dout (rs1_dout),     // output
+    .rs2_dout (rs2_dout),      // output
     .print_reg(print_reg)
   );
 
 
   // ---------- Control Unit ----------
   ControlUnit ctrl_unit (
-    .part_of_inst(),  // input
-    .mem_read(),      // output
-    .mem_to_reg(),    // output
-    .mem_write(),     // output
-    .alu_src(),       // output
-    .write_enable(),  // output
-    .pc_to_reg(),     // output
-    .alu_op(),        // output
-    .is_ecall()       // output (ecall inst)
+    .part_of_inst(IF_ID_inst[6:0]),  // input
+    .mem_read(mem_Read),      // output
+    .mem_to_reg(mem_to_reg),    // output
+    .mem_write(mem_write),     // output
+    .alu_src(alu_src),       // output
+    .write_enable(reg_write),  // output
+    .pc_to_reg(pc_to_reg),     // output
+    .alu_op(alu_op),        // output
+    .is_ecall(is_ecall)       // output (ecall inst)
   );
 
   // ---------- Immediate Generator ----------
   ImmediateGenerator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(IF_ID_inst),  // input
+    .imm_gen_out(imm_gen_out)    // output
   );
 
   // Update ID/EX pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      ID_EX_alu_op <= 0;
+      ID_EX_alu_src <= 0;
+      ID_EX_mem_write <= 0;
+      ID_EX_mem_read <= 0;
+      ID_EX_mem_to_reg <= 0;
+      ID_EX_reg_write <= 0;
+      ID_EX_rs1_data <= 0;
+      ID_EX_rs2_data <= 0;
+      ID_EX_imm <= 0;
+      ID_EX_ALU_ctrl_unit_input <= 0;
+      ID_EX_rd <= 0;
+    end
+    else begin
+      ID_EX_alu_op <= alu_op;
+      ID_EX_alu_src <= alu_src;
+      ID_EX_mem_write <= mem_write;
+      ID_EX_mem_read <= mem_read;
+      ID_EX_mem_to_reg <= mem_to_reg;
+      ID_EX_reg_write <= reg_write;
+      ID_EX_rs1_data <= rs1_dout; 
+      ID_EX_rs2_data <= rs2_dout; 
+      ID_EX_imm <= imm_gen_out;
+      ID_EX_ALU_ctrl_unit_input <= {IF_ID_inst[31:25], IF_ID_inst[14:12], IF_ID_inst[6:0]};
+      ID_EX_rd <= IF_ID_inst[11:7];
     end
     else begin
     end
