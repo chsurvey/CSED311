62eb36a948f9f4fb064666e7ff2d3aee4d4f0ce3
chsurvey <--global>
2025-04-28 01:31:45 +0900

    fetch: Lab4-1 debug left

--- Lab4-1/cpu.v
+++ Lab4-1/cpu.v
@@ -19,6 +19,32 @@ module cpu(input reset,       // positive reset signal
   wire [31:0] rs1_dout;
   wire [31:0] rs2_dout;
   wire [31:0] rd_din;
+  wire is_jal;
+  wire is_jalr;
+  wire branch;
+  wire mem_read;
+  wire mem_to_reg;
+  wire mem_write;
+  wire alu_src;
+  wire write_enable;
+  wire pc_to_reg;
+  wire is_ecall;
+  wire [31:0] imm_gen_out;
+  wire [3:0] alu_op;
+  wire [31:0] alu_in_2;
+  wire [31:0] alu_result;
+  wire alu_bcond;
+  wire pc_src;
+  wire [31:0] mem_dout;
+  reg ID_EX_is_jal;
+  reg ID_EX_is_jalr;
+  reg ID_EX_branch;
+  reg ID_EX_is_branch;
+  reg EX_MEM_is_jump;
+  reg EX_MEM_bcond;
+  reg [31:0] EX_MEM_rs2_data;
+  reg [31:0] EX_MEM_pc_imm_add;
+  reg [31:0] MEM_WB_alu_out;
 
   /***** Register declarations *****/
   // You need to modify the width of registers
@@ -105,15 +131,17 @@ module cpu(input reset,       // positive reset signal
 
 
   // ---------- Control Unit ----------
-  ControlUnit ctrl_unit (
+  control_unit ctrl_unit (
     .part_of_inst(IF_ID_inst[6:0]),  // input
-    .mem_read(mem_Read),      // output
+    .is_jal(is_jal),        // output
+    .is_jalr(is_jalr),       // output
+    .branch(branch),        // output
+    .mem_read(mem_read),      // output
     .mem_to_reg(mem_to_reg),    // output
     .mem_write(mem_write),     // output
     .alu_src(alu_src),       // output
-    .write_enable(reg_write),  // output
+    .write_enable(write_enable),  // output
     .pc_to_reg(pc_to_reg),     // output
-    .alu_op(alu_op),        // output
     .is_ecall(is_ecall)       // output (ecall inst)
   );
 
@@ -126,7 +154,9 @@ module cpu(input reset,       // positive reset signal
   // Update ID/EX pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
-      ID_EX_alu_op <= 0;
+      ID_EX_is_jal <= 0;
+      ID_EX_is_jalr <= 0;
+      ID_EX_branch <= 0;
       ID_EX_alu_src <= 0;
       ID_EX_mem_write <= 0;
       ID_EX_mem_read <= 0;
@@ -139,63 +169,120 @@ module cpu(input reset,       // positive reset signal
       ID_EX_rd <= 0;
     end
     else begin
-      ID_EX_alu_op <= alu_op;
+      ID_EX_is_jal <= is_jal;
+      ID_EX_is_jalr <= is_jalr;
+      ID_EX_branch <= branch;
       ID_EX_alu_src <= alu_src;
       ID_EX_mem_write <= mem_write;
       ID_EX_mem_read <= mem_read;
       ID_EX_mem_to_reg <= mem_to_reg;
-      ID_EX_reg_write <= reg_write;
+      ID_EX_reg_write <= write_enable;
       ID_EX_rs1_data <= rs1_dout; 
       ID_EX_rs2_data <= rs2_dout; 
       ID_EX_imm <= imm_gen_out;
       ID_EX_ALU_ctrl_unit_input <= {IF_ID_inst[31:25], IF_ID_inst[14:12], IF_ID_inst[6:0]};
       ID_EX_rd <= IF_ID_inst[11:7];
     end
-    else begin
-    end
   end
 
   // ---------- ALU Control Unit ----------
   ALUControlUnit alu_ctrl_unit (
-    .part_of_inst(),  // input
-    .alu_op()         // output
+    .part_of_inst(ID_EX_ALU_ctrl_unit_input),  // input
+    .alu_op(alu_op)         // output
   );
 
   // ---------- ALU ----------
+  mux2 alu_mux(
+    .in0 (ID_EX_rs2_data),
+    .in1 (ID_EX_imm),
+    .sel (ID_EX_alu_src),
+    .out (alu_in_2)
+  );
+  
   ALU alu (
-    .alu_op(),      // input
-    .alu_in_1(),    // input  
-    .alu_in_2(),    // input
-    .alu_result(),  // output
-    .alu_zero()     // output
+    .alu_op(alu_op),      // input
+    .alu_in_1(ID_EX_rs1_data),    // input  
+    .alu_in_2(alu_in_2),    // input
+    .alu_result(alu_result),  // output
+    .alu_bcond(alu_bcond)     // output
   );
 
   // Update EX/MEM pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      EX_MEM_is_jump <= 0;
+      EX_MEM_mem_write <= 0;
+      EX_MEM_mem_read <= 0;
+      EX_MEM_bcond <= 0;
+      EX_MEM_is_branch <= 0;
+      EX_MEM_mem_to_reg <= 0;
+      EX_MEM_reg_write <= 0;
+      EX_MEM_alu_out <= 0;
+      EX_MEM_dmem_data <= 0;
+      EX_MEM_rd <= 0;
+      EX_MEM_rs2_data <= 0;
+      EX_MEM_pc_imm_add <= 0;
     end
     else begin
+      EX_MEM_is_jump <= ID_EX_is_jal || ID_EX_is_jalr || ID_EX_branch;
+      EX_MEM_mem_write <= ID_EX_mem_write;
+      EX_MEM_mem_read <= ID_EX_mem_read;
+      EX_MEM_bcond <= alu_bcond;
+      EX_MEM_is_branch <= ID_EX_is_branch;
+      EX_MEM_mem_to_reg <= ID_EX_mem_to_reg;
+      EX_MEM_reg_write <= ID_EX_reg_write;
+      EX_MEM_alu_out <= alu_result; 
+      EX_MEM_dmem_data <= rs2_dout; 
+      EX_MEM_rd <= ID_EX_rd; 
+      EX_MEM_rs2_data <= ID_EX_rs2_data;
+      EX_MEM_pc_imm_add <= current_pc + (ID_EX_imm<<1);
     end
   end
 
+  assign pc_src = EX_MEM_is_jump & EX_MEM_bcond;
+
+  mux2 pc_mux(
+    .in0 (current_pc+4),
+    .in1 (EX_MEM_pc_imm_add),
+    .sel (pc_src),
+    .out (next_pc)
+  );
+
   // ---------- Data Memory ----------
   DataMemory dmem(
-    .reset (),      // input
-    .clk (),        // input
-    .addr (),       // input
-    .din (),        // input
-    .mem_read (),   // input
-    .mem_write (),  // input
-    .dout ()        // output
+    .reset (reset),      // input
+    .clk (clk),        // input
+    .addr (EX_MEM_alu_out),       // input
+    .din (EX_MEM_rs2_data),        // input
+    .mem_read (EX_MEM_mem_read),   // input
+    .mem_write (EX_MEM_mem_write),  // input
+    .dout (mem_dout)        // output
   );
 
   // Update MEM/WB pipeline registers here
   always @(posedge clk) begin
     if (reset) begin
+      MEM_WB_mem_to_reg <= 0;
+      MEM_WB_reg_write <= 0;
+      MEM_WB_mem_to_reg_src_1 <= 0;
+      MEM_WB_mem_to_reg_src_2 <= 0;
+      MEM_WB_alu_out <= 0;
     end
     else begin
+      MEM_WB_mem_to_reg <= EX_MEM_mem_to_reg;
+      MEM_WB_reg_write <= EX_MEM_reg_write;
+      MEM_WB_mem_to_reg_src_1 <= EX_MEM_alu_out; 
+      MEM_WB_mem_to_reg_src_2 <= mem_dout; 
+      MEM_WB_alu_out <= EX_MEM_alu_out;
     end
   end
+
+  mux2 final_mux(
+    .in0(MEM_WB_alu_out),
+    .in1(mem_dout),
+    .sel(MEM_WB_mem_to_reg),
+    .out(rd_din)
+  );
 
   
 endmodule
