cb7674ee9fe0d78ffb9e048cb353643fc9a325ee
chsurvey <--global>
2025-05-13 04:09:51 +0900

    fix: 2bit global saturation predictor

--- Lab4-2/pc_predictor.v
+++ Lab4-2/pc_predictor.v
@@ -18,30 +18,28 @@ module pc_predictor (
     reg [24:0] tag_el;
     reg [31:0] btb_el;
 
-    reg  [1:0]  bht_mem [31:0];
-    reg  [1:0]  bht_el;
+    reg  [1:0] pht;
 
     always @(*) begin
         tag_el = tag_mem[btb_idx];
         btb_el = btb_mem[btb_idx];
-        bht_el = bht_mem[btb_idx];
     end
 
     wire hit;
     wire [31:0] target;
-    assign hit = (tag_el == tag) & bht_el[1];
+    assign hit = (tag_el == tag);
     always @(*) begin
-        pred_taken = hit & btb_el!=0;
+        pred_taken = hit & btb_el!=0 & pht[1];
         pred_pc = pred_taken ? btb_el : (current_pc + 32'd4);
     end
 
     integer i;
     always @(posedge clk) begin
         if (reset) begin
+            pht <= 0;
             for (i = 0; i < 32; i = i + 1) begin
                 tag_mem[i] <= 0;
                 btb_mem[i] <= 0;
-                bht_mem[i] <= 0; // stong not taken
             end
         end
         else if (write_enable) begin      // write_enable
@@ -49,15 +47,15 @@ module pc_predictor (
             btb_mem[acc_btb_idx] <= acc_pc;
         end
 
-        case ({acc_taken, bht_mem[acc_btb_idx]})
-            3'b000: bht_mem[acc_btb_idx] <= 2'b00;
-            3'b001: bht_mem[acc_btb_idx] <= 2'b00;
-            3'b010: bht_mem[acc_btb_idx] <= 2'b01;
-            3'b011: bht_mem[acc_btb_idx] <= 2'b10;
-            3'b100: bht_mem[acc_btb_idx] <= 2'b01;
-            3'b101: bht_mem[acc_btb_idx] <= 2'b10;
-            3'b110: bht_mem[acc_btb_idx] <= 2'b11;
-            3'b111: bht_mem[acc_btb_idx] <= 2'b11;
+        case ({acc_taken, pht})
+            3'b000: pht <= 2'b00;
+            3'b001: pht <= 2'b00;
+            3'b010: pht <= 2'b01;
+            3'b011: pht <= 2'b10;
+            3'b100: pht <= 2'b01;
+            3'b101: pht <= 2'b10;
+            3'b110: pht <= 2'b11;
+            3'b111: pht <= 2'b11;
         endcase
     end
     
